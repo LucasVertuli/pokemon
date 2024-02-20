e1a6c8d98e66a59f46a7264d9f6b22d8ca059678
LucasVertuli <136344537+LucasVertuli@users.noreply.github.com>
2024-02-20 17:05:34 -0300

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero){
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+})
